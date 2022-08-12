ad4dd52220029b4136341d1ada40b215e80bb16d
Xileff <felixsavero78@gmail.com>
2022-08-12 19:57:18 +0700

    Database wrapper completed

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -2,28 +2,25 @@
 
 class Mahasiswa_model
 {
-    private $mhs = [
-        [
-            "nama" => "Felix",
-            "nim" => "412020015",
-            "email" => "felix.412020015@civitas.ukrida.ac.id",
-            "jurusan" => "Teknik Informatika"
-        ],
-        [
-            "nama" => "Albert",
-            "nim" => "412020031",
-            "email" => "albert.412020031@civitas.ukrida.ac.id",
-            "jurusan" => "Teknik Informatika"
-        ],
-        [
-            "nama" => "Andri",
-            "nim" => "412020032",
-            "email" => "andri.412020032@civitas.ukrida.ac.id",
-            "jurusan" => "Teknik Elektro"
-        ]
-    ];
+    private $table = 'mahasiswa';
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
     public function getAllMahasiswa()
     {
-        return $this->mhs;
+        $this->db->query("SELECT * FROM " . $this->table);
+        return $this->db->resultSet();
+    }
+
+    public function getMahasiswaById($id)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+
+        return $this->db->single();
     }
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,17 +1,15 @@
-<?php echo 1 ?>
-
 <div class="container mt-5">
     <div class="row">
         <div class="col-6">
             <h3><?= $data['judul'] ?></h3>
-            <?php foreach ($data['mhs'] as $mhs) : ?>
-                <ul>
-                    <li><?= $mhs['nama'] ?></li>
-                    <li><?= $mhs['nim'] ?></li>
-                    <li><?= $mhs['email'] ?></li>
-                    <li><?= $mhs['jurusan'] ?></li>
-                </ul>
-            <?php endforeach ?>
+            <ul class="list-group">
+                <?php foreach ($data['mhs'] as $mhs) : ?>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <?= $mhs['nama'] ?>
+                        <a href="<?= BASEURL ?>/mahasiswa/detail/<?= $mhs['id'] ?>" class="badge bg-primary">Detail</a>
+                    </li>
+                <?php endforeach ?>
+            </ul>
         </div>
     </div>
 </div>
